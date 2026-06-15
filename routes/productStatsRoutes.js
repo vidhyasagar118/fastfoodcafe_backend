d3db569e7be9d22f8baf9b0fe76245c15896a022
vidhyasagar118 <YOUR_GITHUB_EMAIL>
2026-06-15 14:27:47 +0530

    Fix bestseller stock issue

--- routes/productStatsRoutes.js
+++ routes/productStatsRoutes.js
@@ -1,32 +1,44 @@
-// routes/productStatsRoutes.js
-
 const express = require("express");
 const router = express.Router();
+
 const Order = require("../models/Order");
+const Product = require("../models/Product");
 
 router.get("/bestseller", async (req, res) => {
-  const orders = await Order.find();
+  try {
+    const orders = await Order.find();
 
-  const counts = {};
+    const counts = {};
 
-  orders.forEach((order) => {
-    order.items.forEach((item) => {
-      if (!counts[item._id]) {
-        counts[item._id] = {
-          ...item,
-          sold: 0,
-        };
-      }
+    orders.forEach((order) => {
+      order.items.forEach((item) => {
+        if (!counts[item._id]) {
+          counts[item._id] = 0;
+        }
 
-      counts[item._id].sold += item.qty;
+        counts[item._id] += item.qty;
+      });
     });
-  });
 
-  const products = Object.values(counts)
-    .sort((a, b) => b.sold - a.sold)
-    .slice(0, 6);
+    const productIds = Object.keys(counts);
 
-  res.json(products);
+    const products = await Product.find({
+      _id: { $in: productIds },
+    });
+
+    const result = products.map((product) => ({
+      ...product.toObject(),
+      sold: counts[product._id.toString()] || 0,
+    }));
+
+    result.sort((a, b) => b.sold - a.sold);
+
+    res.json(result.slice(0, 6));
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
 });
 
 module.exports = router;
